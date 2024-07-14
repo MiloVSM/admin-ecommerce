3164c70144a2c8dd040427d7516d177c502a9b1a
Murilo Vieira <murilo1.0@outlook.com>
2024-07-14 00:13:26 -0300

    feat: add basic layout and products page

---
+++ components/Layout.js
@@ -0,0 +1,27 @@
+import { Inter } from "next/font/google";
+import { useSession, signIn, signOut } from "next-auth/react"
+import Nav from "@/components/nav";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Layout({children}) {
+  const { data: session } = useSession()
+  if (!session) { 
+    return (
+      <>
+          <div className={"bg-blue-900 w-screen h-screen flex items-center"}>
+            <div className="text-center w-full">
+              <button onClick={() => signIn('google')} className={"bg-white p-2 px-4 rounded-lg"}>Login with Google</button>
+            </div>
+        </div>
+      </>
+    )
+  }
+  
+  return (
+    <div className="bg-blue-900 min-h-screen flex">
+      <Nav />
+          <div className="bg-white flex-grow rounded-lg p-4">{children}</div>
+    </div>  
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,20 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-import { useSession, signIn, signOut } from "next-auth/react"
-
-const inter = Inter({ subsets: ["latin"] });
+/* eslint-disable @next/next/no-img-element */
+import Layout from "@/components/Layout";
+import { useSession } from "next-auth/react";
 
 export default function Home() {
-  const { data: session } = useSession()
-  if (!session) { 
-    return (
-      <>
-          <div className={"bg-blue-900 w-screen h-screen flex items-center"}>
-            <div className="text-center w-full">
-              <button onClick={() => signIn('google')} className={"bg-white p-2 px-4 rounded-lg"}>Login with Google</button>
-            </div>
-        </div>
-      </>
-    )
-  }
-  
+  const { data: session } = useSession();
   return (
-    <div>logged in {session.user.email}</div>
+    <Layout>
+      <div className="text-blue-900 flex justify-between">
+        <h2>
+          Hello, <b>{session?.user?.name}</b>
+        </h2>
+        <div className="flex gap-2 pr-1 pl-1 text-gray-900 items-center">
+          <img src={session?.user?.image} alt="" className="w-8 h-8 rounded-full"></img>
+          {session?.user?.name}
+        </div>  
+      </div>
+    </Layout>
   )
 }
