a20dec2df0b81e5af9bb32d3e9dac5c153c7d635
Murilo Vieira <murilo1.0@outlook.com>
2024-07-18 08:23:00 -0300

    fix(OAuth): fix page redirection after google OAuth login

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,22 +1,28 @@
-import clientPromise from '@/lib/db'
-import { MongoDBAdapter } from '@auth/mongodb-adapter'
-import NextAuth from 'next-auth'
-import GoogleProvider from 'next-auth/providers/google'
+import clientPromise from "@/lib/db";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const baseUrl = process.env.NEXTAUTH_URL;
 
 export default NextAuth({
   providers: [
     // OAuth authentication providers...
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET
+      clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
   adapter: MongoDBAdapter(clientPromise),
-  debug: true,
+  // debug: true,
   callbacks: {
-    redirect: async (url, baseUrl) => {
-      // Ensuring proper redirect
+    async redirect({ url }) {
+      if (url.startsWith(baseUrl)) {
+        return url;
+      } else if (url.startsWith("/")) {
+        return `${baseUrl}${url}`;
+      }
       return baseUrl;
-    }
-  }
-})
+    },
+  },
+});
